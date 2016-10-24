2f1b0f84672543956138fde2aa2c66a750d0a5fd
torenunez <tore.nunez@gmail.com>
2016-10-23 22:00:20 -0500

    functioning webapp with multiple alerts

--- simple_app.py
+++ simple_app.py
@@ -1,10 +1,21 @@
-from flask import Flask, request
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index(name="Treehouse"):
-    name = request.args.get('name',name)
-    return "Hello from {}".format(name)
+@app.route('/<name>')
+def index(name='Treehouse'):
+    return render_template("index.html", name=name)
+
+
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+def add(num1, num2):
+    context = {'num1': num1, 'num2': num2}
+    return render_template("add.html", **context)
 
 app.run(debug=True, port=8000, host='0.0.0.0')
